a3c44792f6e061f7d07ac6fd0cd1f108cae9a67c
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2024-01-02 18:34:01 -0300

    ✨ create font size tokens

---
+++ Tokens/FontSizeToken.swift
@@ -0,0 +1,9 @@
+protocol FontSizeToken {
+    var xs: UInt8 { get }
+    var sm: UInt8 { get }
+    var md: UInt8 { get }
+    var lg: UInt8 { get }
+    var xl: UInt8 { get }
+    var xxl: UInt8 { get }
+    var xxxl: UInt8 { get }
+}
